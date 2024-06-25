3345c190e920a1dc2bc3316d252dfe3f6749ab76
MegaChie <workspace.mega@gmail.com>
2024-06-25 11:33:09 -0700

    Basic dictionary

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""Task 0"""
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """Basic caching"""
+    def put(self, key, item):
+        """Assign to the dictionary cache_data"""
+        if not key or not item:
+            return
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """Returns the value in self.cache_data linked to passed key"""
+        if not key or key not in self.cache_data.keys():
+            return None
+        return self.cache_data.get(key)
